258336304ceeedfd1212e752942947c3ebf5e2db
Dariya Bengraf <bengraf@MacBook-Air-Bengraf.local>
2021-01-03 23:22:09 +0500

    lazy load

---
+++ NewsRX/Extensions/UIScrollView+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIScrollView+Extensions.swift
+//  NewsRX
+//
+//  Created by Dariya Bengraf on 03.01.2021.
+//  Copyright © 2021 Dariya Bengraf. All rights reserved.
+//
+
+import Foundation
